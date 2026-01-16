7d1d3f297474265052b488871aeada26233d9ff1
kennymai96-cmyk <kennymai96@gmail.com>
2026-01-16 19:33:10 +0000

    initial PA2

--- pa2/Graph.c
+++ pa2/Graph.c
@@ -10,20 +10,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include "Graph.h"
 #include "List.h"
 
 typedef struct GraphObj {
     int order; // define int holding order of graph(# of vertices)
     int arcs; // define int holding # of directed edges
     char* v_label; // define string pointer holding most recent source for BFS
-    
-}
+    List* v_neighbors; // array holding neighbors at each vertex
+    int* v_color; // array holding the color of each vertex
+    int* v_parent; // array holding the parents of each vertex
+    int* v_dist; // array holding the distance from most recent source to vertex
+} GraphObj; //alias
 
 // constructors - destructors -------------------------------------------------
 
 // newGraph()
 // Returns a Graph having n vertices and no edges.
-Graph newGraph(int n);
+Graph newGraph(int n) {
+    Graph G = malloc(sizeof(GraphObj)); //allocate memory for a new Graph
+    assert(G != NULL); // if malloc fails, blow up program;
+    G->order = NULL; 
+    G->arcs = 0; 
+    G->v_label = malloc((G->order + 1) * sizeof(char*));
+    G->v_neighbors = malloc((G->order + 1) * sizeof(List*));
+    G->v_color = malloc((G->order + 1) * sizeof(int*));
+    G->v_parent = malloc((G->order + 1) * sizeof(int*));
+    G->v_dist = malloc((G->order + 1) * sizeof(int*));
+    return G;
+}
 
 // freeGraph()
 // Frees all dynamic memory associated with Graph *pG and sets *pG to NULL.
